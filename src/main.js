e54647d26ce41bf3fc2ac7ea741600427460b7ed
tengfeifei <545881786@qq.com>
2019-02-13 16:38:52 +0800

    gogo

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import MintUI from 'mint-ui'
 import 'mint-ui/lib/style.css'
 
 Vue.config.productionTip = false
+
 Vue.use(MintUI)
 
 new Vue({
--- src/views/shopping.vue
+++ src/views/shopping.vue
@@ -1,9 +1,11 @@
 <template>
   <div id="app">
+   
    <router-view></router-view>
   </div>
 </template>
-
+<script>  
+  </script>
 <style lang="scss">
 
 </style>
