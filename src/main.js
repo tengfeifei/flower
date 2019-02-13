eaef78158aaa5e284f840430107b242f2a9011bb
tengfeifei <545881786@qq.com>
2019-02-13 16:03:31 +0800

    gogo

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
 
 Vue.config.productionTip = false
+Vue.use(MintUI)
 
 new Vue({
   router,
