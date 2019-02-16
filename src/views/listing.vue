971a429b103b08ac45ef7934eee5a99304162231
hou-Star <1523528708@qq.com>
2019-02-16 17:12:48 +0800

    hou

--- src/views/listing.vue
+++ src/views/listing.vue
@@ -10,13 +10,8 @@
 		<ul class="topbar">
 				<router-link to="" tag="li" class="topbar-cell" @click.native="sortByPrice()" :style="isActive.a?'color:red':'color:black'">
 					价格
-<<<<<<< HEAD
 				</router-link>		
 				<router-link to="javascript:;" tag="li" class="topbar-cell" @click.native="sortByDiscount()" :style="isActive.b?'color:red':'color:black'">
-=======
-				</router-link>
-				<router-link to="javascript:;" tag="li" class="topbar-cell">
->>>>>>> af25e7ff1d1e8d7a6022abe7f263f4c66abf3296
 					折扣
 				</router-link>
 				<router-link to="javascript:;" tag="li" class="topbar-cell" @click.native="" :style="isActive?'color:black':'color:red'">
@@ -56,7 +51,6 @@ import { InfiniteScroll } from 'mint-ui'
 Vue.component(Header.name, Header)
 Vue.use(InfiniteScroll)
 export default {
-<<<<<<< HEAD
 	data () {
 		return {
 
@@ -130,37 +124,6 @@ export default {
 	mounted () {
 		this.getProduct();
 	}
-=======
-  data () {
-    return {
-      loading: false,
-      products: [],
-      detailist: ['潮人潮牌', 'aoo', 'app']
-    }
-  },
-  methods: {
-    handleClick (index) {
-      this.$router.push(`/detail/${index}`)
-    },
-    loadMore () {
-					  axios.post('server.html?rpc&method=ClassifyRpc.getCategoryProductInfo&f=www&_=1550144263039', { 'method': 'ClassifyRpc.getCategoryProductInfo', 'params': { 'productIds': '575104186188572,575112414700316,571725431202012,575104166699804,570575752544598,569259431461651,561877113779166,538524029105671,590605132960854,590605023208534,684951497,558766516807560,590605133030486,570553862271945,590605005493334,590605133235286,590605132096598,590604967793750,590605077206102,590605058831446', 'page': 'classify-list-130519-0-0-0-0-1-20.html', 'query': '' }, 'id': 1550135347318, 'jsonrpc': '2.0' }).then(res => {
-					  		this.products = [...this.products, ...res.data['0'].result.data.products]
-					  		console.log(this.products.length)
-					  	})
-    }
-  },
-  mounted () {
-    axios.post('server.html?rpc&method=ClassifyRpc.getCategoryProductInfo&f=www&_=1550144263039', { 'method': 'ClassifyRpc.getCategoryProductInfo', 'params': { 'productIds': '575104186188572,575112414700316,571725431202012,575104166699804,570575752544598,569259431461651,561877113779166,538524029105671,590605132960854,590605023208534,684951497,558766516807560,590605133030486,570553862271945,590605005493334,590605133235286,590605132096598,590604967793750,590605077206102,590605058831446', 'page': 'classify-list-130519-0-0-0-0-1-20.html', 'query': '' }, 'id': 1550135347318, 'jsonrpc': '2.0' }).then(res => {
-      console.log('ok')
-      console.log(res)
-      console.log(res.data)
-      console.log(res.data['0'].result.data.products)
-      this.products = res.data['0'].result.data.products
-      console.log('ccccc', this.products)
-      console.log('ccccc', this.products)
-    })
-  }
->>>>>>> af25e7ff1d1e8d7a6022abe7f263f4c66abf3296
 }
 </script>
 <style lang="scss">
